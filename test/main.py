c9b4fd7cdc09032faa636eeeb532f414afc20ce7
Minh Tran <mtran@nextgen.com>
2019-09-06 15:59:40 -0700

    ROCKSOLID-4428 add tests for Connect docker image, verify Docker container when run with environment variables, volumes, compose, custom extensions and secret

---
+++ test/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import docker
+import unittest
+import argparse
+from utils import DockerUtil
+from test1 import DockerTests1
+from test2 import DockerTests2
+from test3 import DockerTests3
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--image", dest="image", help="docker image name (default: nextgenhealthcare/connect)", required=False)
+    parser.add_argument("-t", "--tag", dest="tag", help="docker image tag (default: latest)", required=False)
+    args = parser.parse_args()
+    image = args.image
+    tag = args.tag
+    tags = [tag]
+    if (not image) and (not tag):
+        # without any argument, will run for all latest JRE images
+        image = "nextgenhealthcare/connect"
+        tags = ["latest","latest-zulu-alpine","latest-adoptopenjdk-openj9-alpine"]
+    elif not image: 
+        image = "nextgenhealthcare/connect"
+    elif not tag:
+        tags = ["latest"]
+
+    test_classes = [DockerTests1, DockerTests2, DockerTests3]
+    loader = unittest.TestLoader()
+
+    for itag in tags:
+        suite = unittest.TestSuite()
+        docker_image = image + ":" + itag
+        for c in test_classes:
+            c.docker_image = docker_image
+            suite.addTests(loader.loadTestsFromTestCase(c))
+        runner = unittest.TextTestRunner(verbosity=2)
+        runner.run(suite)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/utils.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+import docker
+import sys
+import time
+import io
+import tarfile
+import shutil
+import os
+import yaml
+class DockerUtil():
+    @classmethod
+    def generate_compose_yml(cls, destfile, image):
+        # generate yml file used test scenario3
+        with open('./testdata/test.yml','r') as f:
+            data = yaml.safe_load(f)
+        data['services']['mc']['image'] = image
+        # write to destination file, expect full path to file
+        with open(destfile,'w') as f:
+            yaml.dump(data,f,default_flow_style=False)
+
+    @classmethod
+    def wait_for_containers(cls, containers, timeout):
+        # wait 30sec till MC container is up             
+        for container in containers:
+            while not cls.connect_is_up(container):
+                timeout -= 1
+                time.sleep(1)
+                if timeout == 0:
+                    raise Exception("Reached timeout, waiting for Connect to come up")
+
+    @classmethod
+    def connect_is_up(cls, container):
+        # look for "Web server running" line in container log
+        logs = container.logs()
+        if logs.find("Web server running") != -1:
+            return True
+        return False
+
+    @classmethod
+    def check_container_log(cls, container, phrase):
+        # look for string in container log
+        logs = container.logs()
+        if logs.find(phrase) != -1:
+            return True
+        return False
+
+    @classmethod
+    def get_prop_file_as_map(cls, container, path):
+        # retrieve file from container,return value as hash/map 
+        mp = io.BytesIO()
+        bits, stat = container.get_archive(path)
+        for chunk in bits:
+            mp.write(chunk)
+        mp.seek(0)
+        tar = tarfile.open(fileobj=mp)
+        member = tar.getmembers()[0]
+        content = tar.extractfile(member)
+        props = {}
+        for line in content:
+            l = line.strip()
+            if l and not l.startswith("#"):
+                key_value = l.split("=")
+                key = key_value[0].strip()
+                value = "=".join(key_value[1:]).strip().strip('"')
+                props[key] = value
+        tar.close()
+        mp.close()
+        return props
+
+    @classmethod
+    def get_file_as_string_array(cls, container, path):
+        # retrieve file from container, return as string array
+        mp = io.BytesIO()
+        bits, stat = container.get_archive(path)
+        for chunk in bits:
+            mp.write(chunk)
+        mp.seek(0)
+        tar = tarfile.open(fileobj=mp)
+        member = tar.getmembers()[0]
+        content = tar.extractfile(member)
+        array = []
+        for line in content:
+            l = line.strip()
+            if l and not l.startswith("#"):
+                array.append(l)
+        tar.close()
+        mp.close()
+        return array
+
+    @classmethod
+    def get_file_as_string(cls, container, path):
+        # retrieve file from container, return as string
+        mp = io.BytesIO()
+        bits, stat = container.get_archive(path)
+        for chunk in bits:
+            mp.write(chunk)
+        mp.seek(0)
+        tar = tarfile.open(fileobj=mp)
+        member = tar.getmembers()[0]
+        content = tar.extractfile(member)
+        string_content = content.read()
+        tar.close()
+        mp.close()
+        return string_content
+
+    @classmethod
+    def dump_file(cls, container, docker_path, local_path):
+        # retrieve file from cintainer, write to file on local
+        mp = io.BytesIO()
+        bits, stat = container.get_archive(docker_path)
+        for chunk in bits:
+            mp.write(chunk)
+        mp.seek(0)
+        tar = tarfile.open(fileobj=mp)
+        tar.extractall(path=local_path, members=None)
+        tar.close()
+        mp.close()
+
+    @classmethod
+    def list_container_dir(cls, container, docker_path):
+        # retrieve dir from cintainer, return dir listing 
+        mp = io.BytesIO()
+        bits, stat = container.get_archive(docker_path)
+        for chunk in bits:
+            mp.write(chunk)
+        mp.seek(0)
+        dir_tar = tarfile.open(fileobj=mp)
+        dir_list = dir_tar.getnames()
+        dir_tar.close()
+        mp.close()
+        return dir_list
+
+    @classmethod
+    def empty_test_folder(cls, name):
+        # empty test folder on local host
+        local_path = os.getcwd() + "/" + name + "/" 
+        if os.path.isdir(local_path):
+            shutil.rmtree(local_path)
+    
+    @classmethod
+    def create_test_dir(cls, name):
+        # create test folder on local host
+        local_path = os.getcwd() + "/" + name + "/" 
+        directory = os.path.dirname(local_path)
+        print("Create test directory at " + local_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        return local_path
+
+    @classmethod
+    def list_test_dir(cls, name):
+        # list directory content on local host
+        local_path = os.getcwd() + "/" + name + "/" 
+        dirs = os.listdir(local_path)
+        return dirs
+
+    
